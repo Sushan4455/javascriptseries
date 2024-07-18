874024eac0b01ad75d73a02055c404c23ec744b4
Sushan Karki <147791962+Sushan4455@users.noreply.github.com>
2024-07-18 09:53:57 +0000

    conditional

---
+++ conditional.js
@@ -0,0 +1,43 @@
+// if statement
+
+
+// let age = 15;
+
+//  if (age >=  18) {
+//      console.log("you can vote");
+//  }
+
+//  if (age < 18) {
+//     console.log("you cannot vote");
+// }
+
+// let mode = "dark";
+// let color;
+
+// if (mode === "dark") {
+//     color = "black"
+// }
+
+// if (mode === "light") {
+//     color = "white"
+// }
+
+// console.log(color);
+ 
+
+// if else conditional
+
+// let mode = "dark";
+// let color;
+
+// if (mode === "dark") {
+//     color = "black"
+// } else  {
+// color = "white"
+// }
+
+// console.log(color);
+
+// alert("Hello !")
+
+prompt()
