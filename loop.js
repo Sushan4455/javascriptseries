e20ce377a7cabfb8592185f104cd00719ee0ad89
Sushan Karki <147791962+Sushan4455@users.noreply.github.com>
2024-07-18 10:57:30 +0000

    re update

--- loop.js
+++ loop.js
@@ -1,5 +1,24 @@
 // for loop
 
-for(let count=1; count<=5; count++) {
-    console.log("sushan karki");
-}
+// for(let count=1; count<=5; count++) {
+//     console.log("sushan karki");
+// }
+
+// console.log("loop has ended");
+
+//  calculate sum of 1 to 5
+// let sum = 0;
+// for (let i = 1; i <= 5; i++) {
+//     console.log("i=",i );  
+// }
+
+// while loop
+// let i = 1;
+// while (i<=10) {
+//     console.log("i =", i);
+//     i++;
+// }
+
+// do while
+
+
