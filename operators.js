d8ea5d643809f9aa31b703dc19e79fbd7785a24f
Sushan Karki <147791962+Sushan4455@users.noreply.github.com>
2024-07-18 09:21:05 +0000

    operators

---
+++ operators.js
@@ -0,0 +1,36 @@
+// arthimetic operators
+
+// let a =5;
+// let b =2;
+// console.log("a + b =" , a + b)
+// console.log("a - b =" , a - b)
+// console.log("a * b =" , a * b)
+// console.log("a % b =" , a % b)
+// console.log("a / b =" , a / b)
+// console.log("a ** b =" , a ** b)
+
+//unary operators
+
+// let a =5;
+// let b =2;
+// a++;
+// console.log("a =", a );
+// console.log("++a =", ++a );
+ 
+// assignment operators
+
+// let a =5;
+// let b =2;
+
+// a += 4;
+// console.log("a =", a );
+
+// comparision operators
+// console.log("a == b", a == b);
+
+// logical operators
+let a =5;
+let b =2;
+let cond1 = a > b;
+let cond2 = a === 5;
+console.log("cond1 && cond2", cond1 && cond2);
