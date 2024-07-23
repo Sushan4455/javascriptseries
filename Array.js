75c94416ee059352a706ebda6b166cb98902aee0
Sushan Karki <147791962+Sushan4455@users.noreply.github.com>
2024-07-23 11:21:26 +0000

    array

---
+++ Array.js
@@ -0,0 +1,18 @@
+// let marks = [97, 56, 58, 45];
+// console.log(marks);
+
+//  for loop
+
+let people = [ "ironman" , "batman" , "Spiderman" , "hero"]
+// for (let i = 0; i < people.length;i++) {
+//  console.log(people[i]);
+    
+// }
+
+// for of
+
+for(let el of people) {
+    console.log(people);
+}
+
+
