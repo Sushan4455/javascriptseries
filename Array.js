5ff017fb6637a0de64db759b78e74803e7065e5b
Sushan Karki <147791962+Sushan4455@users.noreply.github.com>
2024-07-23 11:44:10 +0000

    re

--- Array.js
+++ Array.js
@@ -3,7 +3,7 @@
 
 //  for loop
 
-let people = [ "ironman" , "batman" , "Spiderman" , "hero"]
+// let people = [ "ironman" , "batman" , "Spiderman" , "hero"]
 // for (let i = 0; i < people.length;i++) {
 //  console.log(people[i]);
     
@@ -11,8 +11,22 @@ let people = [ "ironman" , "batman" , "Spiderman" , "hero"]
 
 // for of
 
-for(let el of people) {
-    console.log(people);
-}
+//  for(let el of people) {
+     // console.log(people);
+//  }
 
+// practice qs
 
+let items = [250, 645, 300, 900, 50]
+ 
+let i = 0;
+ for (let val of items) {
+    console.log(`value of index ${i} = ${val}`);
+    let offer = val / 10;
+    items[i] = items[i] - offer;
+    console.log(`value after offer = ${items[i]}`);
+    i++
+ }
+
+//  let avg = sum / marks.length;
+//  console.log(`avg marks of the class = ${avg}`);
